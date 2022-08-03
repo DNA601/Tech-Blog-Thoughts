d2ecaf06c7fd31a6eef2970def92848bbd2fc8d2
DNA601 <dnavarrete6001@gmail.com>
2022-08-02 20:15:38 -0700

    model user created

--- models/User.js
+++ models/User.js
@@ -1,7 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
-class User extends Model {}
+const sequelize = require('../config/connection');
+
+class User extends Model {
+    checkPassword(loginPw) {
+        return bcrypt.compareSync(loginPw, this.password);
+    }
+}
+
 User.init({
     id: {
         type: DataTypes.INTEGER,
@@ -9,9 +15,17 @@ User.init({
         primaryKey: true,
         autoIncrement: true,
     },
-    username: {
+    name: {
         type: DataTypes.STRING,
         allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+            isEmail: true,
+        },
     },
     password: {
         type: DataTypes.STRING,
@@ -21,9 +35,17 @@ User.init({
         },
     },
 }, {
+    hooks: {
+        beforeCreate: async(newUserData) => {
+            newUserData.password = await bcrypt.hash(newUserData.password, 10);
+            return newUserData;
+        },
+    },
     sequelize,
+    timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'user',
 });
+
 module.exports = User;
